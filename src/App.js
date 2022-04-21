995e1caad88cd6fdf331fc1ea42197f430b77d2f
Jordan Skomal <jskomal@gmail.com>
2022-04-21 00:38:11 -0600

    Import ideaBoxTypes and randomize on load

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import { useState, useEffect } from 'react'
 import { Route } from 'react-router-dom'
 import './App.css'
 import NavBar from './components/NavBar/NavBar'
+import { ideaboxTypes } from './ideaboxTypes'
 
 const App = () => {
+  const [randIdeaboxType, setrandIdeaboxType] = useState(null)
+
   useEffect(() => {
+    getRandIdeaboxType(ideaboxTypes)
+
     const fetchOptions = {
       method: 'GET',
       headers: {
@@ -15,11 +20,31 @@ const App = () => {
     console.log('hi')
   }, [])
 
+  const getRandIdeaboxType = (array) => {
+    let randomIndex = Math.floor(Math.random() * array.length)
+    setrandIdeaboxType(array[randomIndex])
+  }
+
   return (
     <>
       <NavBar />
       <Route exact path='/'>
         <h1 className='main-title'>Idea Ideabox Box</h1>
+        <h2 className='sub-title'>what masterpiece will you make next?</h2>
+        <section className='main-view'>
+          <article className='ideabox-generator'>
+            <h3>{`A THEME-themed`}</h3>
+            <h3>{randIdeaboxType}</h3>
+            <div className='button-pair'>
+              <button>Lock Theme</button>
+              <button>Lock Ideabox Type</button>
+            </div>
+            <div className='button-pair'>
+              <button>Randomize</button>
+              <button>Save Ideabox</button>
+            </div>
+          </article>
+        </section>
       </Route>
     </>
   )
